e88ec3779e57ef9d29d96217c7a5bfeb3493630a
Groupo7 <jameseaster149@gmail.com>
2026-02-17 10:20:36 +0300

    initial Group 07 chat app

---
+++ Desktop/chatting app/chatting_app/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_screen.dart';
+import 'package:chatting_app/users_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          useMaterial3: true, colorSchemeSeed: const Color(0xFF027DFD)),
+      home: const AuthGate(),
+    );
+  }
+}
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+              body: Center(child: CircularProgressIndicator()));
+        }
+        // If user is logged in, go to UsersPage, otherwise LoginPage
+        if (snapshot.hasData)
+          return UsersScreen(receiverId: snapshot.data!.uid);
+        return LoginScreen();
+      },
+    );
+  }
+}
